6daa4acb13586086edeffdfd6756624784631104
nhattanchan <buvghhj123@gmail.com>
2025-03-29 16:29:04 +0700

    commit

--- fe/src/app/not-found.tsx
+++ fe/src/app/not-found.tsx
@@ -7,6 +7,13 @@ export default async function NotFoundPage() {
 
     const t = await getTranslations('notFound')
 
+    if (!t) {
+
+        console.error('Translation notFound is missing!')
+
+        return <div>Error loading translations</div>
+    }
+
     return (
 
         <div className='flex h-full w-full flex-col items-center justify-center'>
